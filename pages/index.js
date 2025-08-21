e4f216a3d151eda3cac7e36601f8230b85401abd
Fábio José Pegoraro <fabiojpegoraro@gmail.com>
2025-08-20 22:21:49 -0300

    Página em construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 function Home() {
-  return <h1>App público(04-08)</h1>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "52rem",
+      }}
+    >
+      <h1>Em construção</h1>
+    </div>
+  );
 }
 
 export default Home;
